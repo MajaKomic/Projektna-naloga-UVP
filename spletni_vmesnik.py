ab055bc7dd04255b18210d858de51a22bc02d4e5
MajaKomic <komic.maja@gmail.com>
2022-08-10 20:23:34 +0200

    Spletnemu vmesniku sem dodala funkcije dodaj semester in dodaj predmet, ter oblikovala html.

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,7 +1,7 @@
 import bottle
 from model import Stanje, Semester, Predmet
 
-IME_DATOTEKE = "primer_stanja.json"
+IME_DATOTEKE = "stanje.json"
 try:
     moje_stanje = Stanje.preberi_iz_datoteke(IME_DATOTEKE)
 except FileNotFoundError:
@@ -9,12 +9,50 @@ except FileNotFoundError:
 #except ValueError:
 #    moje_stanje = Stanje(semestri=[])
 
+def url_semestra(id_semestra):
+    return f"/semester/{id_semestra}/"
+
+    
 @bottle.get("/")
 def osnovna_stran():
     return bottle.template(
         'osnovna_stran.tpl',
         semestri=moje_stanje.semestri
         )
+
+
+@bottle.get("/semester/<id_semestra:int>/")
+def prikazi_semester(id_semestra):
+    #stanje = stanje_trenutnega_uporabnika()
+    semester = moje_stanje.semestri[id_semestra]
+    return bottle.template(
+        "semester.tpl",
+        semestri=moje_stanje.semestri,
+        aktualni_semester=semester,
+        id_aktualni_semester=id_semestra,
+    )
+
+
+@bottle.get("/dodaj-semester/")
+def dodaj_semester_get():
+    return bottle.template(
+        "dodaj_semester.tpl", napake={}, polja={}
+    )
+
+@bottle.post("/dodaj-semester/")
+def dodaj_semester_post():
+    #stanje = stanje_trenutnega_uporabnika()
+    ime = bottle.request.forms.getunicode("ime")
+    semester = Semester(ime, predmeti=[])
+    napake = moje_stanje.preveri_podatke_novega_semestra(semester)
+    if napake:
+        polja = {"ime": ime}
+        return bottle.template("dodaj_semester.tpl", napake=napake, polja=polja)
+    else:
+        id_semestra = moje_stanje.dodaj_semester(semester)
+        #shrani_stanje_trenutnega_uporabnika(stanje)
+        bottle.redirect(url_semestra(id_semestra))
+
 
 @bottle.error(404)
 def error_404(error):
--- views/osnovna_stran.tpl
+++ views/osnovna_stran.tpl
@@ -1,13 +1,21 @@
-<h1>Dobrodošli</h1>
+% rebase('osnova.tpl')
+<!-- Main container -->
+<nav class="level">
+    <div class="level-left">
+        <div class="buttons has-addons field is-horizontal">
+            % for id_semestra, semester in enumerate(semestri):
+            <a href="/semester/{{id_semestra}}/" class="button" name="id_semestra" value="{{id_semestra}}">
+                {{semester.ime_semestra}}
+            </a>
+            % end
+        </div>
 
-<h2>Tukaj je seznam semestrov:</h2>
-<ul>
-% for semester in semestri:
-    <li>
-        {{ semester.ime_semestra}}
-        % for predmet in semester.predmeti:
-            <li>
-                {{ predmet.ime_predmeta}}
-            <\li>
-    </li>
-</ul>
+    </div>
+
+    <div class="level-right">
+            <div class="level-item">
+                <a class="button is-info" href="/dodaj-semester/">dodaj semester</a>
+            </div>
+        </form>
+    </div>
+</nav>
